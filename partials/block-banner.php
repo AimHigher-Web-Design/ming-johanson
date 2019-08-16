ede54b4f015e9ebc66246561436a22798afa7757
Amy Kapernick <amy@aimhigherwebdesign.com>
2019-08-16 11:52:23 +0800

    remove image from banner

--- partials/block-banner.php
+++ partials/block-banner.php
@@ -12,5 +12,5 @@
    <blockquote>
       <?php the_field('text'); ?>
    </blockquote>
-   <?php echo file_get_contents(get_field('image')); ?>
+   <!-- <?php echo file_get_contents(get_field('image')); ?> -->
 </div>
