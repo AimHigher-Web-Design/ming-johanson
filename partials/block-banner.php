f8ef29f17542aa8804921d7fb73c51e359cbbc60
Amy <amy@yowconference.com>
2019-08-21 15:20:41 +0800

    working on parallax and cta sections

--- partials/block-banner.php
+++ partials/block-banner.php
@@ -12,5 +12,12 @@
    <blockquote>
       <?php the_field('text'); ?>
    </blockquote>
-   <!-- <?php echo file_get_contents(get_field('image')); ?> -->
+   <?php 
+      if(preg_match("/\.svg$/", get_field('image'))) {
+         echo file_get_contents(get_field('image')); 
+      }
+      else {
+         echo '<img src="' . get_field('image') . '" />';
+      }
+   ?>
 </div>
