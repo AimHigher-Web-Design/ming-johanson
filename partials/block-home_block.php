621255ecdd1f0cdf05dc07cfac3eb2e85c468885
Amy Kapernick <amy@aimhigherwebdesign.com>
2019-09-02 08:35:04 +0800

    responsive homepage

--- partials/block-home_block.php
+++ partials/block-home_block.php
@@ -10,17 +10,18 @@
 
    <div class="block parallax">
       <img class="paral" src="https://images.unsplash.com/photo-1518946222227-364f22132616?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80" />
-      <?php echo file_get_contents(get_field('image')); ?>
-      <blockquote><?php the_field('heading'); ?></blockquote>
-      <p class="subtitle"><?php the_field('text'); ?></p>
+      <div>
+         <?php echo file_get_contents(get_field('image')); ?>
+         <blockquote><?php the_field('heading'); ?></blockquote>
+         <p class="subtitle"><?php the_field('text'); ?></p>
+      </div>
    </div>
 
-<?php
-
-   if(get_field('call_to_action')) : ?>
+<?php if (get_field('call_to_action')): ?>
       <div class="cta block">
-         <p><?php the_field('cta_text'); ?> <a href="<?php the_field('link'); ?>"><?php the_field('button'); ?></a></p>
+         <p><?php the_field('cta_text'); ?> <a href="<?php the_field('link'); ?>"><?php the_field(
+	'button'
+); ?></a></p>
          
       </div>
-<?php
-   endif; ?>
+<?php endif; ?>
