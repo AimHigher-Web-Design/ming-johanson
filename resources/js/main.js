8851beb8b8f55466949dd1ece3793745ade6f278
Amy Kapernick <amy@aimhigherwebdesign.com>
2019-09-02 10:31:45 +0800

    form styling

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,17 +1,30 @@
 var mobileMenu = function() {
-    if(document.getElementsByClassName('nav-main active').length < 1) {
-        document.getElementById('nav-main').classList.add('active');
-    }
-    else {
-        document.getElementById('nav-main').classList.remove('active');
-    };
-};
+	if (document.getElementsByClassName('nav-main active').length < 1) {
+		document.getElementById('nav-main').classList.add('active')
+	} else {
+		document.getElementById('nav-main').classList.remove('active')
+	}
+}
 
-window.onscroll = function() {
-        if(document.body.scrollTop > 20) {
-            document.getElementById('root').classList.add('scrolled');
-        }
-        else {
-            document.getElementById('root').classList.remove('scrolled');
-        };
-    };
+// window.onscroll = function() {
+//         if(document.body.scrollTop > 20) {
+//             document.getElementById('root').classList.add('scrolled');
+//         }
+//         else {
+//             document.getElementById('root').classList.remove('scrolled');
+//         };
+//     };
+
+document
+	.querySelectorAll(
+		'.wpforms-field-email input, .wpforms-field-text input, .wpforms-field-textarea textarea',
+	)
+	.forEach(i => {
+		i.addEventListener('input', e => {
+			console.log(e.target.value)
+
+			if (e.target.value !== '') {
+				e.target.parentElement.querySelector('label').classList.add('filled')
+			}
+		})
+	})
