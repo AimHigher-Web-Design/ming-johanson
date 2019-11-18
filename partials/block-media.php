de16233ca05f4157cb47e12eda1bcbdf064129b1
Amy Kapernick <amy@aimhigherweb.design>
2019-11-18 16:48:11 +0800

    fixed media items

--- partials/block-media.php
+++ partials/block-media.php
@@ -7,21 +7,33 @@
  * @version 1.0
  */
 
-preg_match(
-	'%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i',
-	get_field('video'),
-	$match
-);
-$youtube_id = $match[1];
+$items = get_field('items');
+if ($items):
+	echo '<ul class="media">';
+
+	while (have_rows('items')):
+
+		the_row();
+		preg_match(
+			'%(?:youtube(?:-nocookie)?\.com/(?:[^/]+/.+/|(?:v|e(?:mbed)?)/|.*[?&]v=)|youtu\.be/)([^"&?/ ]{11})%i',
+			get_sub_field('video'),
+			$match
+		);
+		$youtube_id = $match[1];
+		?>
+
+            <li>
+                <iframe 
+                        
+                        src="https://www.youtube.com/embed/<?php echo $youtube_id; ?>" 
+                        frameborder="0" 
+                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen
+                ></iframe>
+                <p><?php echo the_sub_field('description'); ?></p>
+            </li>
+    <?php
+	endwhile;
+	echo '</ul>';
+endif;
 ?>
-    <div class="media">
-        <iframe 
-           
-            src="https://www.youtube.com/embed/<?php echo $youtube_id; ?>" 
-            frameborder="0" 
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
-        ></iframe>
-        
-        <p><?php echo get_field('description'); ?></p>
-    </div>
